7d96aa7cceb03957a4d8c0d546d1188fd05a1e9b
Eugén Cowie <eugencowie@users.noreply.github.com>
2015-06-01 19:04:05 +0100

    Add player stats

--- SummerProject/src/Components/PlayerInfo.cs
+++ SummerProject/src/Components/PlayerInfo.cs
@@ -2,9 +2,15 @@
 
 namespace SummerProject
 {
-    struct PlayerInfo : IComponent
+    class PlayerInfo : IComponent
     {
         public int PlayerId;
         public bool LocalPlayer;
+
+        public int Level = 1;
+        public int Experience = 0;
+
+        public int BaseHealth = 100;
+        public int BaseMana = 100;
     }
 }
