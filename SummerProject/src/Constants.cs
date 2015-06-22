66967c7b6057ab016513d7b40a4cf1faf8c54408
Eugén Cowie <eugencowie@users.noreply.github.com>
2015-06-22 20:37:18 +0100

    Add initial support for user configurable options

--- SummerProject/src/Constants.cs
+++ SummerProject/src/Constants.cs
@@ -6,5 +6,10 @@
         /// The size of each unit of measurement in the game, in pixels.
         /// </summary>
         public const int UnitSize = 40;
+
+        /// <summary>
+        /// The file path of the options file.
+        /// </summary>
+        public const string OptionsFile = "options.xml";
     }
 }
---
+++ SummerProject/src/Options.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SummerProject
+{
+    [Serializable]
+    public struct OptionData {
+        public int Width;
+        public int Height;
+        public bool Fullscreen;
+        public bool VSync;
+    }
+
+    static class Options
+    {
+        public static OptionData Instance = new OptionData {
+            Width = 800,
+            Height = 600,
+            Fullscreen = false,
+            VSync = true
+        };
+
+        public static void LoadOptionData(string path)
+        {
+            if (!File.Exists(path))
+            {
+                // Use default options.
+                Instance.Fullscreen = true;
+                Instance.Width = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+                Instance.Height = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+                return;
+            }
+
+            using (FileStream f = File.OpenRead(path)) {
+                var xml = new XmlSerializer(Instance.GetType());
+                Instance = (OptionData)xml.Deserialize(f);
+            }
+        }
+
+        public static void WriteOptionData(string path)
+        {
+            string tmpPath = path + "~";
+            using (FileStream f = File.OpenWrite(tmpPath)) {
+                var xml = new XmlSerializer(Instance.GetType());
+                xml.Serialize(f, Instance);
+            }
+
+            if (File.Exists(tmpPath))
+            {
+                if (File.Exists(path))
+                    File.Delete(path);
+
+                File.Move(tmpPath, path);
+            }
+        }
+    }
+}
