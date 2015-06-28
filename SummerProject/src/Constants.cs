905682e87abb56ee8bb54ac589ddacfff484f70d
Eugén Cowie <eugencowie@users.noreply.github.com>
2015-06-28 13:44:58 +0100

    Move extensions to a separate file

--- SummerProject/src/Constants.cs
+++ SummerProject/src/Constants.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.Xna.Framework;
-using System;
-
-namespace SummerProject
+﻿namespace SummerProject
 {
     /// <summary>
     /// This static class contains global static constants which are used in various
@@ -19,36 +16,5 @@ namespace SummerProject
         /// The file path of the options file.
         /// </summary>
         public const string OptionsFile = "options.xml";
-    }
-
-
-    /// <summary>
-    /// This static class contains generic extension methods.
-    /// </summary>
-    static class Extensions
-    {
-        /// <summary>
-        /// Vector2 extension method to convert floating point unit coords to
-        /// integer block coords by rounding the values to the nearest whole
-        /// number and storing them in a Point.
-        /// </summary>
-        public static Point Round(this Vector2 units)
-        {
-            return new Point {
-                X = (int)Math.Round(units.X),
-                Y = (int)Math.Round(units.Y)
-            };
-        }
-
-
-        /// <summary>
-        /// Point extension method to convert integer block coords to floating point
-        /// unit coords. Since this is just a simple int to float conversion, no
-        /// information is lost (unlike the opposite Vector2.Round method above).
-        /// </summary>
-        public static Vector2 ToVector2(this Point block)
-        {
-            return new Vector2(block.X, block.Y);
-        }
     }
 }
---
+++ SummerProject/src/Utilities/Extensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+
+namespace SummerProject
+{
+    /// <summary>
+    /// This static class contains generic extension methods.
+    /// </summary>
+    static class Extensions
+    {
+        /// <summary>
+        /// Vector2 extension method to convert floating point unit coords to
+        /// integer block coords by rounding the values to the nearest whole
+        /// number and storing them in a Point.
+        /// </summary>
+        public static Point Round(this Vector2 units)
+        {
+            return new Point {
+                X = (int)Math.Round(units.X),
+                Y = (int)Math.Round(units.Y)
+            };
+        }
+
+
+        /// <summary>
+        /// Point extension method to convert integer block coords to floating point
+        /// unit coords. Since this is just a simple int to float conversion, no
+        /// information is lost (unlike the opposite Vector2.Round method above).
+        /// </summary>
+        public static Vector2 ToVector2(this Point block)
+        {
+            return new Vector2(block.X, block.Y);
+        }
+    }
+}
